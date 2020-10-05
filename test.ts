0d57f40b6dea2259d51a81b095e4092244a9a4a9
CoolandWild <eric.boileaux@sncf.fr>
2020-10-05 11:03:07 +0200

    creation

---
+++ test.ts
@@ -0,0 +1,14 @@
+function hello(name: string) {
+    console.log("Hello " + name);
+}
+var firstName = "bob";
+
+hello(firstName);
+hello(firstName + " marley");
+
+
+function concat(a:string, b:string) {
+    return a + b;
+}
+var wcs:string  = concat("Wild", concat("Code", "School"));
+console.log(wcs);
